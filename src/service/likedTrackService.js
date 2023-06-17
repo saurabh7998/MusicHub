2b6f78f808b03de8545b18a2a307c5ed84e7a814
saurabh7998 <saurabhgade7998@gmail.com>
2023-06-17 00:50:58 -0400

    changed service url to point to hosted server

--- src/service/likedTrackService.js
+++ src/service/likedTrackService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const SPOTIFY_API = 'http://localhost:4000/api'
+// const SPOTIFY_API = 'http://localhost:4000/api'
+const SPOTIFY_API = 'https://music-hub-service.onrender.com/api'
+
 
 export const likeTrack = async (user) => {
     const response = await axios.post(`${SPOTIFY_API}/${'like'}`, user);
